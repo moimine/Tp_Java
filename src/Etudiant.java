b6f493368a6b0ad9002a6eab3c8364ac232fb41f
moimine <117767714+moimine@users.noreply.github.com>
2024-01-26 02:28:11 +0100

    Update Etudiant.java

--- src/Etudiant.java
+++ src/Etudiant.java
@@ -1,9 +1,10 @@
-import  java.util.Arrays;
+
 public class Etudiant {
     String monNom;
     String monEmail;
     int monId;
     Note[] listDeNotes;
+    //int nbNotes=0
     public Etudiant(String nom,String email, int id){
  this.monId=id;
  this.monNom=nom;
@@ -11,21 +12,50 @@ public class Etudiant {
  this.listDeNotes=new Note[10];
     }
 
-/*
+/* pour ajouter une note:
+
     public void ajouterNote(Note note) {
-            for(int i=0;i<10;i++){
-            listDeNotes[i] = note;
+            if (nbNotes< listDeNotes.length){
+              listDeNotes[nbNotes] = note;
+              nbNotes++;
         }
+
     }
 */
 
-    public String toString(){
-       String str="";
-       for (int i=0;i<10;i++){
-       str+=listDeNotes[i].toString();}
-       return "id: " +monId+ " nom " +monNom+ " email " +monEmail+ " liste de notes " + str;
-//essaie aussi Arrays.toString(listDeNotes)
+    public String toString() {
+        String str="";
+        for (int i = 0; i < listDeNotes.length; i++) {
+            str+=listDeNotes[i];
+        }
+            return "id: " + monId + " nom: " + monNom + " email: " + monEmail + " liste de notes- " + str+ " ";
     }
 
-
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
