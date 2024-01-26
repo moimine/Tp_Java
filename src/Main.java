ac062ef568343725a2e010f490a3861b452f7cc2
moimine <117767714+moimine@users.noreply.github.com>
2024-01-26 02:29:53 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,14 @@ public class Main {
         groupe groupe1=new groupe("TIC-G1");
         System.out.println(groupe1.toString());
 
+/*
 
+        Etudiant etudiant=new Etudiant("Toto","@gmail.com",45);
+        etudiant.ajouterNote(new Note("Physique",4F,18.5F));
+        etudiant.ajouterNote(new Note("mathématiques",5F,19.5F));
+        System.out.println(etudiant.toString());
+
+  */
 
     }
 }
